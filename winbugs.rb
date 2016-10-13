d5643ddbb3431e33fdb7cd8074d4b94b4b90289b
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2016-10-13 13:32:18 -0400

    Updated URLs
    And sha1 -> sha256

--- winbugs.rb
+++ winbugs.rb
@@ -1,24 +1,25 @@
-require 'formula'
 
 class Winbugs < Formula
-  homepage 'http://www.mrc-bsu.cam.ac.uk/bugs/winbugs/contents.shtml'
-  url 'http://www.mrc-bsu.cam.ac.uk/bugs/winbugs/winbugs14.zip'
-  sha1 '8a7c9e548f1680b15edf7b9964bb27c95326db0f'
+  desc "The BUGS (Bayesian inference Using Gibbs Sampling) project."
+  homepage "http://www.mrc-bsu.cam.ac.uk/software/bugs/"
+  url "http://www.mrc-bsu.cam.ac.uk/wp-content/uploads/winbugs14_unrestricted.zip"
+  version "1.4.3"
+  sha256 "f9aa7dc922e991c82b131599ce6aa7f92913d3891c5f05d606ef361a29d198de"
 
-  depends_on 'wine'
+  depends_on "wine"
   depends_on :x11
 
   def install
-    exe = File.new('WinBUGS14', 'w')
+    exe = File.new("WinBUGS14", "w")
     exe << "wine '#{libexec}/WinBUGS14.exe' $@"
     exe.chmod(0777)
     exe.close
-    libexec.install Dir['*']
+    libexec.install Dir["*"]
     bin.mkpath
-    bin.install_symlink '../libexec/WinBUGS14' => 'WinBUGS14'
+    bin.install_symlink "../libexec/WinBUGS14" => "WinBUGS14"
     
-    curl '-O', 'http://www.mrc-bsu.cam.ac.uk/bugs/winbugs/WinBUGS14_cumulative_patch_No3_06_08_07_RELEASE.txt'
-    curl '-O', 'http://www.mrc-bsu.cam.ac.uk/bugs/winbugs/WinBUGS14_immortality_key.txt'
+    curl "-O", "http://www.mrc-bsu.cam.ac.uk/wp-content/uploads/WinBUGS14_cumulative_patch_No3_06_08_07_RELEASE.txt"
+    curl "-O", "http://www.mrc-bsu.cam.ac.uk/wp-content/uploads/WinBUGS14_immortality_key.txt"
     etc.install Dir["WinBUGS14_*.txt"]
   end
 
